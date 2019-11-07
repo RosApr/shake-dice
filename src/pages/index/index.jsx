cd97d18c2d11a5c02a39954bb3d3dff5a2fae1f8
杨小刚 <nulijiuhao712@126.com>
2019-11-07 14:09:41 +0800

    give up animation api

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -13,6 +13,7 @@ export default class Index extends Component {
       selectRange: [...'123456'].map(item => +item),
       dices: null,
       animationList: [],
+      style: {},
     }
   }
   config = {
@@ -20,7 +21,6 @@ export default class Index extends Component {
   }
   shake() {
     const { count, animationList } = this.state
-    // this.resetAnimation()
     const countList = [...Array(count)]
     const dices = countList.map(() => this.createRandomDiceText())
     this.setState(() => {
@@ -29,27 +29,35 @@ export default class Index extends Component {
         animationList: countList.map(() => this.createRandomAnimation()),
       }
     }, () => {
-    this.setState({
-      animationList: animationList.map(ani => ani ? ani.export() : null)
-    })
-    })
-  }
-  resetAnimation() {
-    const { animationList } = this.state
-    this.setState({
-      animationList: animationList.map(ani => {
-        console.log(ani)
-        if(ani) {
-          ani.left('100%').top('20%').step({duration: 0})
-          return ani.export()
-        }
-        return null
+      this.setState({
+        animationList: animationList.map(ani => {
+          console.log(ani)
+          return ani ? ani.export() : null
+        })
       })
     })
   }
+  resetAnimation() {
+    
+  }
   createRandomAnimation() {
-    const animate = Taro.createAnimation()
-    animate.left('20%').top('50%').rotate(Math.random() * 540 - 360).step()
+    const animate = Taro.createAnimation({
+      timingFunction: 'ease-in'
+    })
+    // animate
+    //   .translate(-10, -10)
+    //   .rotate(1)
+    //   .step({duration: 3000})
+    //   .translate(Math.random() * 500 - 200, Math.random() * 500 - 200)
+    //   .rotate(Math.random() * 540 - 360)
+    //   .step({duration: 5000})
+    animate
+      .translate(Math.random() * 500 - 200, Math.random() * 500 - 200)
+      .rotate(Math.random() * 540 - 360)
+      .step({duration: 5000})
+      .translate(-10, -10)
+      .rotate(0)
+      .step({duration: 3000})
     return animate
     // this.setState({
     //     ani: animate
@@ -68,8 +76,11 @@ export default class Index extends Component {
   createRandomDiceText() {
     return Math.round((Math.random() * 5)) + 1
   }
+  test() {
+    console.log('end')
+  }
   render () {
-    const { selectRange, count, dices, animationList } = this.state
+    const { selectRange, count, dices, animationList, style } = this.state
     return (
       <View className='page' onClick={this.shake.bind(this)}>
         <Text>选择骰子数：</Text>
@@ -79,10 +90,22 @@ export default class Index extends Component {
           <View>{count}</View>
         </Picker>
         {dices && dices.map((item, index) => (
-          <View className='dice-container' key={index} animation={animationList[index]}>
+          <View
+            className='dice-container'
+            onTransitionEnd={this.test.bind(this)}
+            key={index}
+            animation={animationList[index]}
+            style={style}
+          >
             <Dice text={item} />
           </View>
         ))}
+        {/* <Dice text={1} />
+        <Dice text={2} />
+        <Dice text={3} />
+        <Dice text={4} />
+        <Dice text={5} />
+        <Dice text={6} /> */}
       </View>
     )
   }
