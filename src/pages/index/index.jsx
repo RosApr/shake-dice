1ddd3e35bbc8797b91a0848052f456777023f322
杨小刚 <nulijiuhao712@126.com>
2019-11-11 12:07:30 +0800

    animation bug

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -42,14 +42,28 @@ export default class Index extends Component {
       }
     }, () => {
       this.setState(() => {
+        console.log('start ani')
         return {
           animationList: animationList.map(ani => {
-            return !ani && ani.export()
+            console.log(ani)
+            return ani ? ani.export() : null
           })
         }
       }, () => {
-        this.setState({
-          animationList: countList.map(() => this.createRandomEndAnimation()),
+        this.setState(() => {
+          return {
+            animationList: this.createRandomEndAnimation(countList),
+          }
+        }, () => {
+          this.setState(() => {
+            console.log('end ani')
+            return {
+              animationList: animationList.map(ani => {
+                console.log(ani)
+                return ani ? ani.export() : null
+              })
+            } 
+          })
         })
       })
     })
@@ -57,39 +71,43 @@ export default class Index extends Component {
   resetAnimation() {
     
   }
-  createRandomEndAnimation() {
-    const randomEndPositionList = this.createRandomEndXY()
-    const animate = Taro.createAnimation({
-      timingFunction: 'ease'
+  createRandomEndAnimation(dices = []) {
+    const endXYList = this.createRandomEndXY()
+    let randomPositionFlag = 5
+    dices.map(() => {
+      const randomI = Math.round(Math.random() * randomPositionFlag)
+      randomPositionFlag -= 1
+      const endP = endXYList.splice(randomI, 1)[0]
+      console.log(endP)
+      const animate = Taro.createAnimation({
+        timingFunction: 'ease'
+      })
+      animate
+        .translate(endP[0], endP[1])
+        .rotate(Math.random() * 540 - 360)
+        .step({duration: 5000})
+      return animate
     })
-    const randomEndX = Math.random() * 500 - 200
-    const randomEndY = Math.random() * 500 - 200
-    animate
-      .translate(randomEndX, randomEndY)
-      .rotate(Math.random() * 540 - 360)
-      .step({duration: 5000})
-    return animate
   }
   createRandomEndXY() {
     const { diceW, windowWidth, windowHeight } = this.state
     const widthGrid = windowWidth / 3
     const heightGrid = windowHeight / 2
-    const diceWidth = diceW
     return [...'012345'].map((val) => {
-      const rX = parseInt(this.getRandomInRange((widthGrid - diceWidth/2) * .6, (widthGrid - diceWidth/2)))
-      const rY = parseInt(this.getRandomInRange((heightGrid - diceWidth/2) * .6, (heightGrid - diceWidth/2)))
+      const rX = parseInt(this.getRandomInRange((widthGrid - diceW/2) * .6, (widthGrid - diceW/2)))
+      const rY = parseInt(this.getRandomInRange((heightGrid - diceW/2) * .6, (heightGrid - diceW/2)))
       if(val == 0) {
         return [rX, rY]
       } else if(val == 1) {
-        return [rX + widthGrid * .8, rY]
+        return [parseInt(rX + widthGrid * .8), rY]
       } else if(val == 2) {
-        return [rX + widthGrid * 1.5, rY]
+        return [parseInt(rX + widthGrid * 1.5), rY]
       } else if(val == 3) {
         return [rX, windowHeight - rY]
       } else if(val == 4) {
-        return [rX + widthGrid  * .8, windowHeight - rY]
+        return [parseInt(rX + widthGrid  * .8), windowHeight - rY]
       } else if(val == 5) {
-        return [rX + widthGrid * 1.5, windowHeight - rY]
+        return [parseInt(rX + widthGrid * 1.5), windowHeight - rY]
       }
     })
   }
@@ -113,14 +131,14 @@ export default class Index extends Component {
       dices: null
     })
   }
+  getRandomInRange(min, max) {
+    return +((Math.random() * ((max-1) - (min+1))).toFixed(2)) + (min+1);
+  }
   createRandomDiceText() {
     return Math.round((Math.random() * 5)) + 1
   }
-  test() {
-    console.log('end')
-  }
   render () {
-    const { selectRange, count, dices, animationList, style } = this.state
+    const { selectRange, count, dices, animationList, diceW } = this.state
     return (
       <View className='page' onClick={this.shake.bind(this)}>
         <Text>选择骰子数：</Text>
@@ -132,12 +150,11 @@ export default class Index extends Component {
         </Picker>
         {dices && dices.map((item, index) => (
           <View
-            style={{width: , height: ,}}
+            style={{width: `${diceW}px`, height: `${diceW}px`,}}
             className='dice-container'
             onTransitionEnd={this.test.bind(this)}
             key={index}
             animation={animationList[index]}
-            style={style}
           >
             <Dice text={item} />
           </View>
